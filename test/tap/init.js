68a8ffe14b93073f2352e9f82d85055177293539
Shane Osbourne <shane.osbourne8@gmail.com>
2015-08-31 22:48:21 +0100

    tests for online + port resolution

--- test/tap/init.js
+++ test/tap/init.js
@@ -1,10 +1,28 @@
 'use strict';
 
 var bs      = require('../../');
-var request = require('supertest');
+var Rx      = require('rx');
+//var request = require('supertest');
 var test    = require('../tap').test;
 var utils   = require('../utils');
+var sinon   = require('sinon');
+var isOnline   = require('../../lib/online');
 
-test('E2E server test with only a callback', function (t) {
-    t.type(bs.create, 'function');
+test('init with online option', function (t) {
+    t.plan(4);
+    bs.create({online: false}, function (err, bs) {
+        t.equal(bs.options.get('online'), false);
+    });
+    bs.create({online: true}, function (err, bs) {
+        t.equal(bs.options.get('online'), true);
+    });
+    var stub = sinon.stub(isOnline, 'online').returns(Rx.Observable.just(false));
+    bs.create({}, function (err, bs) {
+        t.equal(bs.options.get('online'), false);
+    });
+    stub.returns(Rx.Observable.just(true));
+    bs.create({}, function (err, bs) {
+        t.equal(bs.options.get('online'), true);
+    });
+    isOnline.online.restore();
 });
