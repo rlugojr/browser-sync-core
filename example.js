3a451612d288205ea6adf2a9cff95e39aedb4ec2
Shane Osbourne <shane.osbourne8@gmail.com>
2015-09-01 15:53:36 +0100

    Accept and resolve plugins in multiple formats

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ var bs = require('./')
     .create({
         port: 4000,
         plugins: [
-            "test/fixtures/plugin1.js",
+            "./test/fixtures/plugin1",
             {
                 module: {
                     init: function (bs, opts, done) {
@@ -11,13 +11,20 @@ var bs = require('./')
                             done();
                         }, 1000);
                     },
-                    "plugin:name": "My Awesome Plugin"
+                    "plugin:name": "My Awesome Plugin",
+                    "ui": true
                 },
                 options: {
                     name: "shane"
                 }
+            },
+            {
+                module: "connect",
+                options: {
+                    name: "kittie"
+                }
             }
         ]
     }, function (err, out) {
-        console.log(out.options.get('plugins').toJS());
+        console.log(out.options.get('plugins').get(0));
     });
