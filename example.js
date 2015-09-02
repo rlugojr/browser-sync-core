be8476f5c1938ce4cb52a6bc8d0cc8a3e69f034e
Shane Osbourne <shane.osbourne8@gmail.com>
2015-09-02 07:45:37 +0100

    use new externalIps option

--- example.js
+++ example.js
@@ -1,33 +1,42 @@
-var bs = require('./')
-    .create({
-        version: "2.8.2",
-        port: 4000,
-        plugins: [
-            "./test/fixtures/plugin1",
-            {
-                module: {
-                    init: function (bs, opts, done) {
-                        setTimeout(function () {
-                            console.log('Setup!');
-                            done();
-                        }, 1000);
-                    },
-                    "plugin:name": "My Awesome Plugin",
-                    "ui": true
-                },
-                options: {
-                    name: "shane"
-                }
-            },
-            {
-                module: "connect",
-                options: {
-                    name: "kittie"
-                }
-            }
-        ]
-    }, function (err, out) {
-        console.log(out.options.get('urls').toJS());
-        console.log(out.options.get('snippet'));
-        console.log(out.options.get('scriptPaths'));
-    });
+var url = require('url');
+
+var obj = {
+    hostname: "localhost",
+    scheme: "http:",
+};
+
+console.log(url.format(obj));
+
+//var bs = require('./')
+//    .create({
+//        version: "2.8.2",
+//        port: 4000,
+//        plugins: [
+//            "./test/fixtures/plugin1",
+//            {
+//                module: {
+//                    init: function (bs, opts, done) {
+//                        setTimeout(function () {
+//                            console.log('Setup!');
+//                            done();
+//                        }, 1000);
+//                    },
+//                    "plugin:name": "My Awesome Plugin",
+//                    "ui": true
+//                },
+//                options: {
+//                    name: "shane"
+//                }
+//            },
+//            {
+//                module: "connect",
+//                options: {
+//                    name: "kittie"
+//                }
+//            }
+//        ]
+//    }, function (err, out) {
+//        console.log(out.options.get('urls').toJS());
+//        console.log(out.options.get('snippet'));
+//        console.log(out.options.get('scriptPaths'));
+//    });
--- test/tap/options/urls.js
+++ test/tap/options/urls.js
@@ -10,7 +10,10 @@ test('init with urls option', function (t) {
     }, function (err, bs) {
         var urls = bs.options.get('urls').toJS();
         var port = bs.options.get('port');
-        t.equal(urls.local, 'http://localhost:' + port);
+        t.equal(urls.local,    'http://localhost:' + port);
+        if (bs.options.get('online')) {
+            t.ok(urls.external.match(new RegExp(port + '$')));
+        }
         t.end();
         //t.equal(plugins.length, 1);
         //t.equal(plugins[0].options.name, 'shane');
