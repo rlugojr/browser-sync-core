64a05fa0b669f16c39bef04be64acfd520e7212b
Shane Osbourne <shane.osbourne8@gmail.com>
2015-09-04 21:17:47 +0100

    Add multiple files plugins

--- test/mocha/options/files.js
+++ test/mocha/options/files.js
@@ -35,16 +35,27 @@ describe('init with core files option', function () {
                     }
                 },
                 {
-                    module: {init: function () {
+                    module: {
+                        init: function () {
 
-                    }},
+                        }
+                    },
+                    options: {
+                        files: ["*.html"]
+                    }
                 }
             ]
         }, function (err, bs) {
+
             var files = bs.options.get('files').toJS();
+            var last  = files[Object.keys(files).slice(-1)];
+
             assert.deepEqual(files.core.globs, ['*.css']);
             assert.deepEqual(files['Plugin1'].globs, ['*.html', '*.jade']);
             assert.deepEqual(files['Plugin1'].objs[0].match, '*.txt');
+
+            assert.deepEqual(last.globs[0], '*.html');
+            assert.deepEqual(last.objs.length, 0);
             done();
         });
     });
