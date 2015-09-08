0ddae79cc91d5eac2388962fb781d587d22b920f
Shane Osbourne <shane.osbourne8@gmail.com>
2015-09-08 14:05:00 +0100

    Merge middlewares from plugin hooks

--- test/mocha/options/middlewares.js
+++ test/mocha/options/middlewares.js
@@ -4,15 +4,42 @@ var bs       = require('../../../');
 var sinon    = require('sinon');
 var assert   = require('chai').assert;
 
-describe('middlewares as options', function () {
+describe.only('middlewares as options', function () {
     it('accepts top-level middleware', function (done) {
-        var spy = sinon.spy();
-        var fn = function () {};
+        var fn1 = function fn1() {};
+        var fn2 = function fn2() {};
+        var fn3 = function fn3() {};
+        var fn4 = function fn4() {};
+        var fn5 = function fn5() {};
+        var fn6 = function fn6() {};
         bs.create({
-            middleware: [fn]
+            middleware: [fn1, fn2],
+            plugins: [
+                {
+                    module: {
+                        hooks: {
+                            "server:middleware": [fn3, fn4]
+                        }
+                    }
+                },
+                {
+                    module: {
+                        hooks: {
+                            "server:middleware": [fn5, fn6]
+                        }
+                    }
+                }
+            ]
         }, function (err, bs) {
             var middleware = bs.options.get('middleware').toJS();
-            assert.equal(middleware[0], fn);
+            var plugins    = bs.options.get('plugins').toJS();
+            assert.equal(middleware[0], fn1);
+            assert.equal(middleware[1], fn2);
+            assert.equal(middleware[2], fn3);
+            assert.equal(middleware[3], fn4);
+            assert.equal(middleware[4], fn5);
+            assert.equal(middleware[5], fn6);
+            assert.equal(middleware.length, 6);
             done();
         });
     });
