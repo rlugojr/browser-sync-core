9476dcc750f564fc31b96fee0c174f351f2eaf15
Shane Osbourne <shane.osbourne8@gmail.com>
2015-09-23 17:22:43 +0100

    cleanup recursive verifier

--- test/mocha/protocol/Global.js
+++ test/mocha/protocol/Global.js
@@ -1,16 +1,68 @@
 var assert = require('chai').assert;
 var proto  = require('../../../lib/protocol');
 
-describe('Protocol: Global', function () {
+function runOne (arg) {
+    return proto.send('Global.inject', arg);
+}
 
-    it('can collect errors', function () {
-        console.log(proto.send('Global.inject', {
-            basename: 'style.css',
+describe('Protocol: Global', function () {
+    //it('can collect inject errors', function () {
+    //    assert.equal(runOne({}).errors.length, 3);
+    //});
+    //it('can collect inject errors', function () {
+    //    var actual = runOne({
+    //        ext: '.css'
+    //    });
+    //    assert.equal(actual.errors.length, 2);
+    //});
+    //it('can collect inject errors', function () {
+    //    var actual = runOne({
+    //        ext: '.css',
+    //        basename: 'styles.css'
+    //    });
+    //    assert.equal(actual.errors.length, 1);
+    //});
+    //it('can collect inject errors', function () {
+    //    var actual = runOne({
+    //        ext: '.css',
+    //        basename: 'styles.css',
+    //        item: {}
+    //    });
+    //    assert.equal(actual.errors.length, 0);
+    //});
+    //it('can collect inject errors', function () {
+    //    var actual = runOne({
+    //        ext: '.css',
+    //        basename: 'styles.css',
+    //        item: {
+    //            locator: {}
+    //        }
+    //    });
+    //    assert.equal(actual.errors.length, 4);
+    //});
+    //it('can collect inject errors', function () {
+    //    var actual = runOne({
+    //        ext: '.css',
+    //        basename: 'styles.css',
+    //        item: {
+    //            locator: {
+    //                source: '(.+)?.css'
+    //            }
+    //        }
+    //    });
+    //    assert.equal(actual.errors.length, 3);
+    //});
+    it.only('can collect inject errors', function () {
+        var actual = runOne({
+            ext: '.css',
+            basename: 'styles.css',
             item: {
                 locator: {
-                    name: "shane"
+                    source: '(.+)?.css',
+                    global: true
                 }
             }
-        }))
+        });
+        console.log(actual);
     });
 });
