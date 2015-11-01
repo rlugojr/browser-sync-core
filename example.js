e53ce9c78b554b741e83204b32c76dc06069c0be
Shane Osbourne <shane.osbourne8@gmail.com>
2015-11-01 20:28:20 +0000

    Bump resp-modifier to 6.0.0 to allow easier proxy res rewrites

--- example.js
+++ example.js
@@ -1,23 +1,16 @@
-var Rx        = require('rx');
-var sub       = new Rx.Subject();
-var subStream = sub.publish().refCount().distinctUntilChanged();
-var sub2      = new Rx.Subject();
-var subStream2 = sub2.publish().refCount().distinctUntilChanged();
+var Rx = require('rx');
 
-var count = 0;
-setInterval(function () {
-    console.log('    sent:', count += 1);
-    sub.onNext(count);
-}, 1000);
+var source = Rx.Observable.range(0, 3)
+    .map(function (x) { return Rx.Observable.range(x, 3); })
+    .switch();
 
-
-var count2 = 0;
-setInterval(function () {
-    console.log('  2 sent:', count2 += 10);
-    sub2.onNext(count2);
-}, 1000);
-
-subStream
-    .combineLatest(subStream2)
-    .do(x => console.log('received:', x))
-    .subscribe();
+var subscription = source.subscribe(
+    function (x) {
+        console.log('Next: ' + x);
+    },
+    function (err) {
+        console.log('Error: ' + err);
+    },
+    function () {
+        console.log('Completed');
+    });
