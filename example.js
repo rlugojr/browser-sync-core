d229d355e7fdddc0796d19623efb9eb016d5ed8a
Shane Osbourne <shane.osbourne8@gmail.com>
2015-11-05 07:07:00 +0000

    Spiking out client tracking

--- example.js
+++ example.js
@@ -1,16 +1,71 @@
 var Rx = require('rx');
+var Immutable = require('immutable');
 
-var source = Rx.Observable.range(0, 3)
-    .map(function (x) { return Rx.Observable.range(x, 3); })
-    .switch();
+var clients = Immutable.fromJS({
+    'default': createClient({id: 'default'}),
+    'client1': createClient({id: '123456'})
+});
 
-var subscription = source.subscribe(
-    function (x) {
-        console.log('Next: ' + x);
+var users = [
+    {
+        id: 'client2'
     },
-    function (err) {
-        console.log('Error: ' + err);
+    {
+        id: 'client3'
     },
-    function () {
-        console.log('Completed');
+    {
+        id: 'client4'
+    }
+];
+
+var clients$ = new Rx.BehaviorSubject(clients);
+
+var connections$ = Rx.Observable.create(function (obs) {
+    var count = 0;
+	var int = setInterval(function () {
+		obs.onNext(users[count]);
+        count += 1;
+        if (count === users.length) {
+            count = 0;
+        }
+	}, 1050);
+}).share();
+
+connections$
+    .withLatestFrom(clients$, (connection, clients) => {
+        return {
+            connection: connection,
+            clients: clients
+        }
+    })
+    .map(x => {
+        if (x.clients.hasIn([x.connection.id])) {
+            console.log('EXISTS:', x.connection.id);
+            return x.clients.update(x.connection.id, function (client) {
+            	return client.set('heartbeat', new Date().getTime());
+            })
+        } else {
+            return x.clients.set(x.connection.id, createClient(x.connection));
+        }
+        return x.clients;
+    })
+    .subscribe(clients$);
+
+function createClient (incoming) {
+    return Immutable.Map({
+        id: incoming.id,
+        heartbeat: new Date().getTime()
     });
+}
+
+var source = Rx.Observable
+    .interval(1000)
+    .withLatestFrom(clients$, (_, x) => x)
+    .do(function (clients) {
+        console.log('--~~--~~--~~--~~--~~--~~--~~--~~--~~--');
+        console.log(clients.size);
+    	//clients.toList().forEach(function (val, i) {
+    	//	console.log('index:', i, 'id:', val.get('id'), 'hb', val.get('heartbeat'));
+    	//})
+    })
+    .subscribe();
