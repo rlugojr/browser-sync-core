20bf85934289bca868dd75980b31e52395b0b9c0
Shane Osbourne <shane.osbourne8@gmail.com>
2015-11-09 21:38:40 +0000

    Use socket.io's tracking via packets for reliable connection info

--- example.server.js
+++ example.server.js
@@ -3,7 +3,8 @@ var bs = require('./');
 bs.create({
     serveStatic: 'test/fixtures',
     externals: {
-        clientJs: '/Users/shaneobsourne/sites/browser-sync-client'
+        //clientJs: '/Users/shaneobsourne/sites/browser-sync-client',
+        clientJs: '/Users/shakyshane/code/bs-client'
     }
 }, function (err, bs) {
     console.log(bs.options.get('urls'));
