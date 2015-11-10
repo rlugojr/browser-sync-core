811c07d496827f3cf3cf8b3649f11deb9a9923c9
Shane Osbourne <shane.osbourne8@gmail.com>
2015-11-10 17:34:03 +0000

    Add client stream that is browser-session aware

--- example.server.js
+++ example.server.js
@@ -4,8 +4,10 @@ bs.create({
     serveStatic: 'test/fixtures',
     externals: {
         //clientJs: '/Users/shaneobsourne/sites/browser-sync-client',
-        clientJs: '/Users/shakyshane/code/bs-client'
-    }
+        //clientJs: '/Users/shakyshane/code/bs-client'
+        clientJs: '/Users/shakyshane/sites/oss/browser-sync-client/' // home imac
+    },
+    minify: false
 }, function (err, bs) {
     console.log(bs.options.get('urls'));
 });
