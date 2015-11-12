bf46e7e0629556bf4af56328cf311dd9c8325b06
Shane Osbourne <shane.osbourne8@gmail.com>
2015-11-12 16:14:13 +0000

    Send browser-specific data back on initial Client.register websocket event

--- example.server.js
+++ example.server.js
@@ -1,12 +1,13 @@
 var bs = require('./');
 
 bs.create({
-    serveStatic: 'test/fixtures',
-    plugins: ['/Users/shakyshane/Sites/browser-sync-modules/browser-sync-cp'],
+    serveStatic: ['test/fixtures'],
+    //plugins: ['/Users/shakyshane/Sites/browser-sync-modules/browser-sync-cp'],
+    plugins: ['/Users/shakyshane/sites/oss/UI'],
     externals: {
         //clientJs: '/Users/shaneobsourne/sites/browser-sync-client'
-        clientJs: '/Users/shakyshane/code/bs-client'
-        //clientJs: '/Users/shakyshane/sites/oss/browser-sync-client/' // home imac
+        //clientJs: '/Users/shakyshane/code/bs-client'
+        clientJs: '/Users/shakyshane/sites/oss/browser-sync-client/' // home imac
     },
     minify: false
 }, function (err, bs) {
