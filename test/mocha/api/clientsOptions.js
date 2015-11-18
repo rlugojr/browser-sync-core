280d8d68a5b3b8e2e990a1869f557fb272527272
Shane Osbourne <shane.osbourne8@gmail.com>
2015-11-18 16:57:53 +0000

    Add test for setting initial client options

---
+++ test/mocha/api/clientsOptions.js
@@ -0,0 +1,23 @@
+const browserSync = require('../../../');
+const files       = require('../../../lib/files');
+const utils       = require('../utils');
+const conf        = require('../../../lib/config');
+const assert      = require('chai').assert;
+
+describe('Client options', function () {
+    it('adds the default options to new clients', function (done) {
+        browserSync.create({}, function (err, bs) {
+            const client = utils.getClientSocket(bs);
+            client.emit(conf.events.CLIENT_REGISTER, utils.getClient('123456'));
+            bs.clients$.skip(1)
+                .take(1)
+                .toArray()
+                .subscribe(function (clients) {
+                    const first = clients[0].get('123456').toJS();
+                    assert.equal(first.options.reloadOnRestart, false);
+                    bs.cleanup();
+                    done();
+                }, err => done(err));
+        });
+    });
+});
