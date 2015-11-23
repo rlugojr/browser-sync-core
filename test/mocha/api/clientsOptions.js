2f003b708b72ce57409e14057e9b1249c0cd73de
Shane Osbourne <shane.osbourne8@gmail.com>
2015-11-23 21:51:27 +0000

    Add tests for individual client option updating

--- test/mocha/api/clientsOptions.js
+++ test/mocha/api/clientsOptions.js
@@ -7,80 +7,70 @@ const Rx          = require('rx');
 const Observable  = Rx.Observable;
 
 describe('Client options', function () {
-    it('adds the default options to new clients', function (done) {
+    it('creates a new client with default options', function (done) {
         browserSync.create({}, function (err, bs) {
             const client = utils.getClientSocket(bs);
             client.emit(conf.events.CLIENT_REGISTER, utils.getClient('123456'));
-            bs.clients$.skip(1)
-                .take(1)
-                .toArray()
-                .subscribe(function (clients) {
-                    const first = clients[0].get('123456').toJS();
-                    assert.equal(first.options.reloadOnRestart, false);
+            bs.clients$.take(2).toArray()
+                .subscribe(clients => {
+                    const path1 = ['123456', 'options', 'reloadOnRestart'];
+                    assert.equal(clients[1].getIn(path1), false);
                     bs.cleanup();
                     done();
-                }, err => done(err));
+                });
         });
     });
-    it('updates a single clients options', function (done) {
+    it('creates a new client with default options, then a second with updated', function (done) {
         browserSync.create({}, function (err, bs) {
+
             const client = utils.getClientSocket(bs);
 
             client.emit(conf.events.CLIENT_REGISTER, utils.getClient('123456'));
 
-            bs.clients$.skip(1).take(1).subscribe(function (client) {
-            	assert.equal(
-                    client.getIn(['123456', 'options', 'reloadOnRestart']),
-                    false
-                );
-                listen();
-            });
-
-            function listen() {
-                bs.clients$
-                    .skip(1)
-                    .take(1)
-                    .subscribe(function (clients) {
-                        assert.equal(
-                            clients.getIn(['123456', 'options', 'reloadOnRestart']),
-                            true
-                        );
-                        bs.cleanup();
-                        done();
-                    });
-
-                bs.setClientOption('123456', ['reloadOnRestart'], (value) => {
-                    return !value;
-                });
-            }
-        });
-    });
-    it('updates the default client options so that new connections get the updated value', function (done) {
-        browserSync.create({}, function (err, bs) {
-            bs.setDefaultClientOption(['reloadOnRestart'], () => {
-                return true;
-            }).subscribe(x => {
-                assert.equal(x.getIn(['clientOptions', 'reloadOnRestart']), true);
-                connect();
-            });
-
-            function connect () {
-
-                const client = utils.getClientSocket(bs);
-
-                client.emit(conf.events.CLIENT_REGISTER, utils.getClient('123456'));
-
-                bs.clients$.skip(1).take(1).subscribe(function (client) {
-                    assert.equal(
-                        client.getIn(['123456', 'options', 'reloadOnRestart']),
-                        true
-                    );
+            bs.clients$.take(3).toArray()
+                .subscribe(clients => {
+                    assert.equal(clients[1].size, 1);
+                    assert.equal(clients[2].size, 2);
+                    assert.equal(clients[2].getIn(['123456', 'id']), '123456');
+                    assert.equal(clients[2].getIn(['654321', 'id']), '654321');
+                    assert.equal(clients[2].getIn(['123456', 'options', 'reloadOnRestart']), false); // default
+                    assert.equal(clients[2].getIn(['654321', 'options', 'reloadOnRestart']), 'shane');
+                    bs.cleanup();
                     done();
-                }, err => done(err));
-            }
+                });
+
+            bs.registered$.take(1)
+                .flatMap(() => {
+                    return bs.setDefaultClientOption('reloadOnRestart', x => 'shane')
+                })
+                // add a second client, after updating default options
+                .do(x => client.emit(conf.events.CLIENT_REGISTER, utils.getClient('654321')))
+                .subscribe()
         });
     });
-    it.only('overrides a previously set client option with an override', function (done) {
+    it('sets a single option on a client', function (done) {
+        browserSync.create({}, function (err, bs) {
+
+            const client = utils.getClientSocket(bs);
+            client.emit(conf.events.CLIENT_REGISTER, utils.getClient('123456'));
+
+            bs.clients$.take(3).toArray()
+                .subscribe(clients => {
+                    const path1 = ['123456', 'options', 'reloadOnRestart'];
+                    assert.equal(clients[1].getIn(path1), false); // default
+                    assert.equal(clients[2].getIn(path1), 'shane');
+                    bs.cleanup();
+                    done();
+                });
+
+            bs.registered$.take(1).flatMap(x => {
+                return Observable.concat([
+                    bs.setClientOption('123456', 'reloadOnRestart', x => 'shane')
+                ])
+            }).subscribe();
+        });
+    });
+    it('overrides a previously set client option with an override', function (done) {
         browserSync.create({}, function (err, bs) {
 
             const client = utils.getClientSocket(bs);
@@ -88,10 +78,11 @@ describe('Client options', function () {
 
             bs.clients$.take(4).toArray()
                 .subscribe(clients => {
-                    var path = ['123456', 'options', 'notify'];
+                    const path = ['123456', 'options', 'notify'];
                     assert.equal(clients[1].getIn(path), true);
                     assert.equal(clients[2].getIn(path), 'shane');
                     assert.equal(clients[3].getIn(path), 'kittie');
+                    bs.cleanup();
                     done();
                 });
 
