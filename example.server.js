28f9c9cd3fe07fb03b94d652ad9773339888871b
Shane Osbourne <shane.osbourne8@gmail.com>
2015-11-25 17:30:37 +0000

    Adding proxy example

--- example.server.js
+++ example.server.js
@@ -1,15 +1,28 @@
 var bs = require('./');
 
 bs.create({
-    serveStatic: ['test/fixtures'],
-    plugins: ['/Users/shakyshane/Sites/browser-sync-modules/browser-sync-cp'],
+    //serveStatic: ['test/fixtures'],
+    proxy: {target: 'http://sunspel.static/'},
+    scheme: 'https',
+    rewriteRules: [
+        {
+            match: 'fast.fonts.net',
+            replace: 'shane is ace',
+            paths: ['/assets/css/core.min.css**']
+        }
+    ],
+    plugins: [
+        './lib/proxy',
+        '/Users/shakyshane/Sites/browser-sync-modules/browser-sync-cp'
+    ],
     //plugins: ['/Users/shakyshane/sites/oss/UI'],
+    //plugins: ['./lib/proxy'],
     externals: {
         //clientJs: '/Users/shaneobsourne/sites/browser-sync-client'
         clientJs: '/Users/shakyshane/code/bs-client'
         //clientJs: '/Users/shakyshane/sites/oss/browser-sync-client/' // home imac
-    },
-    minify: false
+    }
+    //minify: false
 }, function (err, bs) {
     console.log(bs.options.get('urls'));
 });
