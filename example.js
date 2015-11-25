43456a4b6e58415d8ca8fb603ae2778e83f309e9
Shane Osbourne <shane.osbourne8@gmail.com>
2015-11-25 07:44:48 +0000

    Add setOption api

--- example.js
+++ example.js
@@ -1,5 +1,10 @@
-var bs = require('./lib/browser-sync');
-bs.create({}, function (err, bs) {
-    console.log('cleanup');
-    bs.cleanup();
-});
+//var bs = require('./lib/browser-sync');
+//bs.create({}, function (err, bs) {
+//    console.log('cleanup');
+//    bs.cleanup();
+//});
+var O = require('rx').Observable;
+
+O.just('shane')
+    .combineLatest(O.just('kittie'))
+    .subscribe(x => console.log(x));
