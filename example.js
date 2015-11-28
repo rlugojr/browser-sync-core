b611e1c876ac6a947d38addeda09cb477b396748
Shane Osbourne <shane.osbourne8@gmail.com>
2015-11-28 21:28:35 +0000

    remove cookie dependency

--- example.js
+++ example.js
@@ -1,10 +1,16 @@
-//var bs = require('./lib/browser-sync');
-//bs.create({}, function (err, bs) {
-//    console.log('cleanup');
-//    bs.cleanup();
-//});
-var O = require('rx').Observable;
+////var bs = require('./lib/browser-sync');
+////bs.create({}, function (err, bs) {
+////    console.log('cleanup');
+////    bs.cleanup();
+////});
+//var O = require('rx').Observable;
+//
+//O.just('shane')
+//    .combineLatest(O.just('kittie'))
+//    .subscribe(x => console.log(x));
 
-O.just('shane')
-    .combineLatest(O.just('kittie'))
-    .subscribe(x => console.log(x));
+var cookie = require('cookie');
+var util = require('./lib/proxy-utils');
+console.log(
+    util.rewriteCookies('logged_in=no; domain=.github.com; path=/; expires=Wed, 28 Nov 2035 10:43:12 -0000; secure; HttpOnly')
+);
--- example.server.js
+++ example.server.js
@@ -2,25 +2,27 @@ var bs = require('./');
 
 bs.create({
     //serveStatic: ['test/fixtures'],
-    proxy: {target: 'http://sunspel.static/'},
-    scheme: 'https',
+    proxy: {target: 'http://www.bbc.co.uk'},
+    //scheme: 'https',
     rewriteRules: [
-        {
-            match: 'fast.fonts.net',
-            replace: 'shane is ace',
-            paths: ['/assets/css/core.min.css**']
-        }
+        //{
+            //match: /info\.sunspel\.dev/g,
+            //fn: function (req, res, match) {
+            //    return 'http://' + req.headers['host'];
+            //}
+        //}
     ],
     plugins: [
         './lib/proxy',
-        '/Users/shakyshane/Sites/browser-sync-modules/browser-sync-cp'
+        //'/Users/shakyshane/Sites/browser-sync-modules/browser-sync-cp'
+        '/Users/shakyshane/sites/oss/UI'
     ],
     //plugins: ['/Users/shakyshane/sites/oss/UI'],
     //plugins: ['./lib/proxy'],
     externals: {
         //clientJs: '/Users/shaneobsourne/sites/browser-sync-client'
-        clientJs: '/Users/shakyshane/code/bs-client'
-        //clientJs: '/Users/shakyshane/sites/oss/browser-sync-client/' // home imac
+        //clientJs: '/Users/shakyshane/code/bs-client'
+        clientJs: '/Users/shakyshane/sites/oss/browser-sync-client/' // home imac
     }
     //minify: false
 }, function (err, bs) {
