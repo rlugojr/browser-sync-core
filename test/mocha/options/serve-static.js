4161ab58854cf31e05b56469e751b0159169eb9a
Shane Osbourne <shane.osbourne8@gmail.com>
2016-01-18 07:46:41 +0000

    Allow serve static with multiple roots

--- test/mocha/options/serve-static.js
+++ test/mocha/options/serve-static.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var bs       = require('../../../');
-var sinon    = require('sinon');
 var assert   = require('chai').assert;
 var ss       = require('../../../lib/opt.serve-static');
 var merge    = require('../utils').optmerge;
@@ -10,7 +8,7 @@ function process(conf) {
     return ss.transformOptions(ss.merge(merge(conf)));
 }
 
-describe.only('serveStatic as options', function () {
+describe('serveStatic as options', function () {
     it('accepts a single string', function () {
         var result = process({
             serveStatic: './css'
@@ -29,8 +27,8 @@ describe.only('serveStatic as options', function () {
             .toJS();
 
         assert.equal(result.length, 2);
-        assert.equal(result[0].root, './css');
-        assert.equal(result[1].root, './app');
+        //assert.equal(result[0].root, './css');
+        //assert.equal(result[1].root, './app');
     });
     it('accepts a single object', function () {
         var result = process({
@@ -69,57 +67,58 @@ describe.only('serveStatic as options', function () {
         assert.equal(result[1].namespace, 'My SS');
         assert.equal(result[1].options.extensions, 'here');
     });
-    it.only('accepts an array of objects', function () {
+    it('accepts an array of objects', function () {
         var result = process({
-            serveStatic: [{
-                root: ['./css', './app'],
-                options: {
-                    extensions: 'here'
+            serveStatic: [
+                {
+                    root: ['./css', './app'],
+                    options: {
+                        extensions: 'here'
+                    },
+                    namespace: 'My SS'
                 },
-                namespace: 'My SS'
-            }]
+                {
+                    root: 'tmp',
+                    namespace: 'My Second attempt'
+                },
+            ]
         })
             .get('serveStatic')
             .toJS();
 
-        console.log(result);
-        assert.equal(result.length, 2);
-        //assert.equal(result[0].root, './css');
-        //assert.equal(result[0].namespace, 'My SS');
-        //assert.equal(result[1].root, './app');
-        //assert.equal(result[1].namespace, 'My SS');
-        //assert.equal(result[1].options.extensions, 'here');
+        assert.equal(result.length, 3);
+        assert.equal(result[0].root, './css');
+        assert.equal(result[0].namespace, 'My SS');
+        assert.equal(result[0].options.extensions, 'here');
+        assert.equal(result[1].root, './app');
+        assert.equal(result[1].namespace, 'My SS');
+        assert.equal(result[1].options.extensions, 'here');
+        assert.equal(result[2].root, 'tmp');
+        assert.equal(result[2].namespace, 'My Second attempt');
     });
-    //it('accepts array with object + strings', function () {
-    //    var result = process({
-    //        serveStatic: [
-    //            {
-    //                root: ['./css', './tmp'],
-    //                options: {
-    //                    name: 'shane'
-    //                }
-    //            }
-    //        ]
-    //    })
-    //        .get('serveStatic')
-    //        .toJS();
-    //
-    //    assert.equal(result.length, 3);
-    //    assert.equal(result[0].root, './css');
-    //    assert.equal(result[0].options.name, 'shane');
-    //    assert.equal(result[1].root, './tmp');
-    //    assert.equal(result[1].options.name, 'shane');
-    //    assert.equal(result[2].root, './test/fixtures');
-    //});
-    //it('accepts multi roots with shared options', function () {
-    //    var ss = process({
-    //        serveStatic: [
-    //            {
-    //                root: ['./app']
-    //            }
-    //        ]
-    //    }).get('serveStatic').toJS();
-    //    assert.equal(ss.length, 1);
-    //    assert.equal(ss[0].root, './app');
-    //});
+    it('accepts array with both objects + strings', function () {
+        var result = process({
+            serveStatic: [
+                {
+                    root: ['./css', './tmp'],
+                    options: {
+                        name: 'shane'
+                    }
+                },
+                'test/fixtures'
+            ]
+        })
+            .get('serveStatic')
+            .toJS();
+
+        assert.equal(result.length, 3);
+        assert.equal(result[0].root, './css');
+        assert.equal(result[0].options.name, 'shane');
+        assert.equal(result[1].root, './tmp');
+        assert.equal(result[1].options.name, 'shane');
+
+        assert.equal(result[2].root, 'test/fixtures');
+        assert.equal(result[2].namespace, 'core');
+        assert.deepEqual(result[2].options, {});
+    });
 });
